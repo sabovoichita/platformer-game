4a1c2aa19db7cd9bcc6cc8efac628be9a25c9913
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-02 19:07:45 +0100

    41. The requestAnimationFrame() web API, takes in a callback and is used to update the animation on the screen. The animate function will be responsible for updating the player's position and continually drawing it on the canvas. Inside the animate function, call the requestAnimationFrame() API and pass animate as the argument.

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ class Player {
 
 const player = new Player();
 
-const animate = () => {};
+const animate = () => {
+  requestAnimationFrame(animate);
+};
 
 const startGame = () => {
   canvas.style.display = "block";
