cb764b789cff723a2737e2efe9521e558cac7572
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-02 19:12:17 +0100

    45. Inside the keys object, add a new key called rightKey and assign it an object with the key-value pair of pressed: false. Below the rightKey object, create a leftKey object and assign it an object with the key-value pair of pressed: false.

--- index.js
+++ index.js
@@ -59,7 +59,10 @@ const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
-  const keys = {};
+  const keys = {
+    rightKey: { pressed: false },
+    leftKey: { pressed: false },
+  };
 };
 
 const startGame = () => {
