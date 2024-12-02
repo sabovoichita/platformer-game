fc2153f77650c64fa20f30347a90adf2c08de797
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-02 18:14:47 +0100

    12. The next step is to define some characteristics for the main player of the game. Start by creating a new class called Player.

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
   return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 };
+
+class Player {}
