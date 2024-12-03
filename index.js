25960c85b150f6dc24382cce6e9362cc4cce6845
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 09:48:00 +0100

    61. Add another addEventListener to the global window object and pass in the keyup event and use destructuring to pass in the key property from the event.

--- index.js
+++ index.js
@@ -115,3 +115,4 @@ startBtn.addEventListener("click", startGame);
 window.addEventListener("keydown", ({ key }) => {
   movePlayer(key, 8, true);
 });
+window.addEventListener("keyup", ({ key }) => {});
