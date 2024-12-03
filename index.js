0c006832dfa0735276cf48f28decb4937e100b83
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 15:50:54 +0100

    111. Add another boolean expression that checks if the player's position.y is greater than or equal to the checkpoint's position.y. Below that statement, add another boolean expression that checks if the player's position.y plus the player's height is less than or equal to the checkpoint's position.y plus the checkpoint's height. Below that statement, add the isCheckpointCollisionDetectionActive variable.

--- index.js
+++ index.js
@@ -188,6 +188,10 @@ const animate = () => {
   checkpoints.forEach((checkpoint, index, checkpoints) => {
     const checkpointDetectionRules = [
       player.position.x >= checkpoint.position.x,
+      player.position.y >= checkpoint.position.y,
+      player.position.y + player.height <=
+        checkpoint.position.y + checkpoint.height,
+      isCheckpointCollisionDetectionActive,
     ];
   });
 };
