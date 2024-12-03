693173822871c75c296891fac966ab7b2e27e263
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 09:30:22 +0100

    56. Below your if statement, use the subtraction assignment operator to subtract the xVelocity from player.velocity.x. To close out this case, make sure to add a break statement.

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ const movePlayer = (key, xVelocity, isPressed) => {
       if (xVelocity === 0) {
         [(player.velocity.x = xVelocity)];
       }
+      player.velocity.x -= xVelocity;
+      break;
   }
 };
 
