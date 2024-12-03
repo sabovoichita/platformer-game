52b691061b6fbe0a7c80520ea856bce867de6fa6
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 12:20:31 +0100

    83. Add another boolean expression that checks if the sum of the player's y position, height, and y velocity is greater than or equal to the platform's y position.

--- index.js
+++ index.js
@@ -125,6 +125,8 @@ const animate = () => {
   platforms.forEach((platform) => {
     const collisionDetectionRules = [
       player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >=
+        platform.position.y,
     ];
   });
 };
