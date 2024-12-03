fc5d53fd44698a94cbdf62d56c15af2c962e88e6
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 15:41:47 +0100

    108. Create an if statement that checks if isCheckpointCollisionDetectionActive is true. Inside the if statement, add a setTimeout() that takes in a callback function and a delay of 2000 milliseconds.For the callback function, it should set the checkpointScreen style.display property to "none".

--- index.js
+++ index.js
@@ -234,6 +234,11 @@ const startGame = () => {
 const showCheckpointScreen = (msg) => {
   checkpointScreen.style.display = "block";
   checkpointMessage.textContent = msg;
+  if (isCheckpointCollisionDetectionActive) {
+    setTimeout(() => {
+      checkpointScreen.style.display = "none";
+    }, 2000);
+  }
 };
 
 startBtn.addEventListener("click", startGame);
