835b09ff5ba311511ff0a6ce201824f882f84048
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-03 09:47:22 +0100

    60. Inside the arrow function, call the movePlayer function and pass in key, 8, and true as arguments.

--- index.js
+++ index.js
@@ -112,4 +112,6 @@ const startGame = () => {
 
 startBtn.addEventListener("click", startGame);
 
-window.addEventListener("keydown", ({ key }) => {});
+window.addEventListener("keydown", ({ key }) => {
+  movePlayer(key, 8, true);
+});
